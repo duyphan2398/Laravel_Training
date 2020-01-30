55c767f0c77cf4d1fbf9490f9280b2937e272bb7
Phan Nhat Duy <49288733+duyphan2398@users.noreply.github.com>
2020-01-30 16:15:40 +0700

    Revert "Merge pull request #2 from duyphan2398/feature/mySchool"
    This reverts commit 61466d26f27ba23a7f10012f0fcac2593fafe951.

--- mySchool/app/Subject.php
+++ mySchool/app/Subject.php
@@ -3,8 +3,6 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class Subject extends Model
 {
@@ -14,9 +12,4 @@ class Subject extends Model
     public function teacher(){
         return $this->belongsTo('App\Teacher','id_teacher','id');
     }
-    protected $fillable = [
-        'name',
-        'credit',
-        'id_teacher'
-    ];
 }
--- mySchool/resources/views/layout/layout.blade.php
+++ mySchool/resources/views/layout/layout.blade.php
@@ -12,24 +12,11 @@
     <title>@yield('title')</title>
 </head>
 <body>
-    @if( Session::has('status'))
-    <div style="z-index: 2; position:relative;" class="alert alert-success alert-dismissible fade show" role="alert">
-        <strong>{{Session::get('status')}}</strong>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-
-    @endif
-
-    <div style="z-index: 0">
-        @yield('content')
-    </div>
+    @yield('content')
 
     <footer class="text-center">
         <hr class="bg-dark mt-5">
         <h1>THIS IS MY FOOTER</h1>
     </footer>
-
 </body>
 </html>
